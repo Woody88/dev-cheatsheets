62306347d9d7288e20743cf86fa1d7ba88ceb736
Woodson Delhia <woodsondelhia88@gmail.com>
2018-06-24 03:17:41 -0400

    implemented route change on browser url change

--- src/Main.purs
+++ src/Main.purs
@@ -2,19 +2,39 @@ module Main where
 
 import Prelude
 
+import Control.Coroutine as CR
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
+import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-import Routing.PushState (makeInterface)
 import Router as R
+import Routing.PushState (makeInterface)
+import Simple.JSON (write)
 
 
 main :: Effect Unit
 main = HA.runHalogenAff $ do
   nav  <- liftEffect $ makeInterface
+  
   body <- HA.awaitBody
   location <- liftEffect $ nav.locationState
-  void $ liftEffect $ log location.path        --- Debug purposes
-  runUI R.router location.path body
+  void $ liftEffect $ log location.path        --- Debug purpose
+  
+  app <- runUI R.router location.path body
+  
+  _ <- liftEffect $ nav.listen $ browserChangeHandler (app.query)
+
+  app.subscribe $ CR.consumer \(R.RouteChanged path) -> do
+    liftEffect $ nav.pushState (write {}) path
+    pure Nothing
+  
+  where                                                        
+    browserChangeHandler qry location = do
+      log location.path
+      launchAff_ $ qry $ H.action $ (R.LocationChange location.path)
+    
+      
--- src/Router.purs
+++ src/Router.purs
@@ -4,28 +4,31 @@ import Prelude
 
 import Data.Either (either)
 import Data.Foldable (oneOf)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String (stripPrefix, Pattern(..))
 import Effect.Aff (Aff)
-import Effect.Console (log)
 import Halogen as H
-import Halogen as HA
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Routing (match)
-import Routing.Match (Match, lit, end)
-import Web.HTML.HTMLDocument.ReadyState (parse)
-
+import Routing.Match (Match, lit, root, end)
 
 data Query a 
     = Goto Route a
-    | Location String a
+    | LocationChange String a
+
+data Message 
+    = RouteChanged String
 
 data Route
     = Home
+    | Page
     | NotFound
+    
 
 instance showRoutes :: Show Route where
   show (Home)     = "Home"
+  show (Page)     = "Page"
   show (NotFound) = "NotFound"
 
 type State =
@@ -33,24 +36,28 @@ type State =
   }
 
 parseRoute :: String -> Route
-parseRoute = parseRouteEither <<< match routing
+parseRoute = parseRouteEither <<< match routing <<< stripPrefixBackslash
     where parseRouteEither = either (const NotFound) identity
+          stripPrefixBackslash "/" = "/"
+          stripPrefixBackslash str = fromMaybe "404" $ stripPrefix (Pattern "/") str
 
 routing :: Match Route
 routing = oneOf
     [ home
+    , page
     , pure NotFound
     ]
-    where home = Home <$ lit "" <* end
-         
+    where 
+          home = Home <$ root <* end
+          page = Page <$ lit "page" <* end
 
-router :: H.Component HH.HTML Query String Void Aff
+router :: H.Component HH.HTML Query String Message Aff
 router = 
     H.component
         { initialState: (\path -> { currentPage: show $ parseRoute path })
         , render
         , eval
-        , receiver: HE.input Location  -- Not sure if this is ever called, need some clarification
+        , receiver: const Nothing 
         }
   where
     render :: State -> H.ComponentHTML Query
@@ -62,17 +69,24 @@ router =
 
     viewPage :: String -> H.ComponentHTML Query
     viewPage path = case path of
-        "Home" -> HH.div_ [ HH.button [ HE.onClick (HE.input_ $ Goto NotFound) ] [ HH.text "Not Found" ] ]
+        "Home" -> HH.div_ [ HH.button [ HE.onClick (HE.input_ $ Goto Page) ] [ HH.text "Not Found" ] ]
+        "Page" -> HH.div_ [ HH.p_ [] ]
         _      -> HH.p_ [] 
     
-    eval :: Query ~> H.ComponentDSL State Query Void Aff
+    eval :: Query ~> H.ComponentDSL State Query Message Aff
     eval (Goto NotFound next) = do
       void $  H.modify (\s -> s { currentPage = "NotFound" })
       pure next
+    eval (Goto Page next) = do
+      void $  H.modify (\s -> s { currentPage = "Page" })
+      H.raise $ RouteChanged "/page"
+      pure next
     eval (Goto Home next) = do
       void $  H.modify (\s -> s { currentPage = "Home" })
+      H.raise $ RouteChanged "/"
       pure next
-    eval (Location path next) = do
-       HA.liftEffect $ log $ "Path: " <> path
-       eval $ Goto (parseRoute path) next
-    
+    eval (LocationChange str next) = do
+      void $  H.modify (\s -> s { currentPage = show $ parseRoute str })
+      pure next
+
+      
