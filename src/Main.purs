ab462fd6e8453a5c2d82b6012556ef29236bfeca
Woodson Delhia <woodsondelhia88@gmail.com>
2018-06-23 20:10:41 -0400

    Routing impletement, need to implement push state next

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,18 @@ module Main where
 import Prelude
 
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Routing.PushState (makeInterface)
 import Router as R
+
 
 main :: Effect Unit
 main = HA.runHalogenAff $ do
+  nav  <- liftEffect $ makeInterface
   body <- HA.awaitBody
-  runUI R.router unit body
+  location <- liftEffect $ nav.locationState
+  void $ liftEffect $ log location.path        --- Debug purposes
+  runUI R.router location.path body
--- src/Router.purs
+++ src/Router.purs
@@ -2,41 +2,55 @@ module Router where
   
 import Prelude
 
+import Data.Either (either)
+import Data.Foldable (oneOf)
 import Data.Maybe (Maybe(..))
--- import Effect.Aff (Aff)
+import Effect.Aff (Aff)
+import Effect.Console (log)
 import Halogen as H
--- import Halogen.Aff as HA
+import Halogen as HA
 import Halogen.HTML as HH
--- import Halogen.HTML.Properties as HP
--- import Routing (match)
-import Routing.Match (Match, lit)
+import Halogen.HTML.Events as HE
+import Routing (match)
+import Routing.Match (Match, lit, end)
+import Web.HTML.HTMLDocument.ReadyState (parse)
 
 
 data Query a 
-    = Goto Routes a
+    = Goto Route a
+    | Location String a
 
-data Routes 
+data Route
     = Home
+    | NotFound
+
+instance showRoutes :: Show Route where
+  show (Home)     = "Home"
+  show (NotFound) = "NotFound"
 
 type State =
   { currentPage :: String
   }
 
-init :: State
-init = { currentPage: "Home" }
+parseRoute :: String -> Route
+parseRoute = parseRouteEither <<< match routing
+    where parseRouteEither = either (const NotFound) identity
 
-routing :: Match Routes
-routing = home
-    where home = Home <$ lit ""
+routing :: Match Route
+routing = oneOf
+    [ home
+    , pure NotFound
+    ]
+    where home = Home <$ lit "" <* end
+         
 
-
-router :: forall m. H.Component HH.HTML Query Unit Void m
+router :: H.Component HH.HTML Query String Void Aff
 router = 
     H.component
-        { initialState: const init
+        { initialState: (\path -> { currentPage: show $ parseRoute path })
         , render
         , eval
-        , receiver: const Nothing
+        , receiver: HE.input Location  -- Not sure if this is ever called, need some clarification
         }
   where
     render :: State -> H.ComponentHTML Query
@@ -48,10 +62,17 @@ router =
 
     viewPage :: String -> H.ComponentHTML Query
     viewPage path = case path of
-        "Home" -> HH.div_ []
+        "Home" -> HH.div_ [ HH.button [ HE.onClick (HE.input_ $ Goto NotFound) ] [ HH.text "Not Found" ] ]
         _      -> HH.p_ [] 
     
-    eval :: Query ~> H.ComponentDSL State Query Void m
+    eval :: Query ~> H.ComponentDSL State Query Void Aff
+    eval (Goto NotFound next) = do
+      void $  H.modify (\s -> s { currentPage = "NotFound" })
+      pure next
     eval (Goto Home next) = do
       void $  H.modify (\s -> s { currentPage = "Home" })
       pure next
+    eval (Location path next) = do
+       HA.liftEffect $ log $ "Path: " <> path
+       eval $ Goto (parseRoute path) next
+    
